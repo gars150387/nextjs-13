f44bfd72426910106c6fc004dc1f3567072572d5
Gustavo <gar.santeliz@gmail.com>
2021-12-03 20:37:40 -0500

    add router and switch and route to navegation

---
+++ gars_website/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { CarouselMainPage} from './components/Carousel';
+import {NavBarMain} from "./components/NavBar";
+import {Router,Switch,Route} from "react-router-dom"
+
+
+function App() {
+  return (
+    <Router>
+          <div className="App">
+      <Switch>
+      <NavBarMain />
+      <Route to="/">
+      <CarouselMainPage />
+      </Route>
+      {/* <Route to="/">
+      <CarouselMainPage />
+      </Route>
+      <Route to="/">
+      <CarouselMainPage />
+      </Route>
+      <Route to="/">
+      <CarouselMainPage />
+      </Route> */}
+      </Switch>
+    </div>
+
+    </Router>
+  );
+}
+
+export default App;
+
