c811508a9a2c3afc2db1c2fdb49a3adf96e729f8
Gustavo <gar.santeliz@gmail.com>
2021-12-04 14:10:17 -0500

    text position center

--- gars_website/src/components/Footer.js
+++ gars_website/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 export const Footer =()=>{
     return (
         <div className="footer">
-            <p className="text-center mt-4 mb-4">
+            <p className="text-center" id="text1">
                 Rights reserved by Gustavo A. Rodriguez S.
             </p>
         </div>
