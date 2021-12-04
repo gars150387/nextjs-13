1621310e01216fc0c2cb589076a07bb7f2e6af0f
Gustavo <gar.santeliz@gmail.com>
2021-12-04 13:55:13 -0500

    footer component added

---
+++ gars_website/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const Footer =()=>{
+    return (
+        <div>
+            <p>
+                Rights reserved by Gustavo A. Rodriguez S.
+            </p>
+        </div>
+    )
+}
