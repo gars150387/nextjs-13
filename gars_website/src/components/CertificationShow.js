59560cdbd22ad0a531a014390295f01654a4c55d
Gustavo <gar.santeliz@gmail.com>
2021-12-06 18:58:46 -0500

    add images

--- gars_website/src/components/CertificationShow.js
+++ gars_website/src/components/CertificationShow.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import CertificationsData from '../../src/components/CertificationsData'
+// import CertificationsData from '../../src/components/CertificationsData'
 import BecomeAReactDeveloper from "../images/BecomeAReactDeveloper.jpg";
 import BuildAGoogleMapAppWithJavascript from "../images/BuildAGoogleMapAppWithJavascript.jpg";
 import FirebaeEssentialTraining from "../images/FirebaeEssentialTraining.jpg";
@@ -10,8 +10,8 @@ import ReactEcosystems from "../images/ReactEcosystems.jpg";
 import ReactHooks from "../images/ReactHooks.jpg";
 import ReactTestingAndDebugging from "../images/ReactTestingAndDebugging.jpg";
 import ReactEssentialTraining from "../images/ReactEssentialTraining.jpg";
-// import NodejsEssentialTraining from "../images/NodejsEssentialTraining.jpg";
-// import DevelopingACriticalThinkingMindset from "../images/DevelopingACriticalThinkingMindset.jpg";
+import NodejsEssentialTraining from "../images/NodejsEssentialTraining.jpg";
+import DevelopingACriticalThinkingMindset from "../images/DevelopingACriticalThinkingMindset.jpg";
 import FullStackWebDeveloperCertificationMiamiDadeCollege from "../images/FullStackWebDeveloperCertificationMiamiDadeCollege.jpg";
 import ReactDesignPatterns from "../images/ReactDesignPatterns.jpg";
 import ReactCreatingAndHostingAFullStackSite from "../images/ReactCreatingAndHostingAFullStackSite.jpg";
@@ -20,12 +20,21 @@ import ReactCreatingAndHostingAFullStackSite from "../images/ReactCreatingAndHos
 export const CertificationShow = () => {
 	return (
 		<>
-                {CertificationsData.map((item)=>{
-                    return (
-                        <img src={BecomeAReactDeveloper} key={item.id}/>
-                    )
-                })}
-
+        <img className="d-block mt-2 mb-2 w-75 h-75" src={BecomeAReactDeveloper} />
+        <img className="d-block mt-2 mb-2 w-75 h-75" src={BuildAGoogleMapAppWithJavascript} />
+        <img className="d-block mt-2 mb-2 w-75 h-75" src={FirebaeEssentialTraining} />
+        <img className="d-block mt-2 mb-2 w-75 h-75" src={JavascriptCodeChallenges} />
+        <img className="d-block mt-2 mb-2 w-75 h-75" src={ReactBuildingAnInterface} />
+        <img className="d-block mt-2 mb-2 w-75 h-75" src={ReactComponentsContextAndAccessibility} />
+        <img className="d-block mt-2 mb-2 w-75 h-75" src={ReactEcosystems} />
+        <img className="d-block mt-2 mb-2 w-75 h-75"  src={ReactHooks} />
+        <img className="d-block mt-2 mb-2 w-75 h-75"  src={ReactTestingAndDebugging} />
+        <img className="d-block mt-2 mb-2 w-75 h-75"  src={ReactEssentialTraining} />
+        <img className="d-block mt-2 mb-2 w-75 h-75"  src={NodejsEssentialTraining} />
+        <img className="d-block mt-2 mb-2 w-75 h-75"  src={DevelopingACriticalThinkingMindset} />
+        <img className="d-block mt-2 mb-2 w-75 h-75"  src={FullStackWebDeveloperCertificationMiamiDadeCollege} />
+        <img className="d-block mt-2 mb-2 w-75 h-75"  src={ReactDesignPatterns} />
+        <img className="d-block mt-2 mb-2 w-75 h-75"  src={ReactCreatingAndHostingAFullStackSite} />
         </>
     )
 }
