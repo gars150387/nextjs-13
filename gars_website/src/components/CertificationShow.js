32857b7a4d144b9ad6fdcc2530f9be59121028dd
Gustavo <gar.santeliz@gmail.com>
2021-12-06 19:23:44 -0500

    trying to render data

--- gars_website/src/components/CertificationShow.js
+++ gars_website/src/components/CertificationShow.js
@@ -1,40 +1,32 @@
-import React from 'react'
-// import CertificationsData from '../../src/components/CertificationsData'
-import BecomeAReactDeveloper from "../images/BecomeAReactDeveloper.jpg";
-import BuildAGoogleMapAppWithJavascript from "../images/BuildAGoogleMapAppWithJavascript.jpg";
-import FirebaeEssentialTraining from "../images/FirebaeEssentialTraining.jpg";
-import JavascriptCodeChallenges from "../images/JavascriptCodeChallenges.jpg";
-import ReactBuildingAnInterface from "../images/ReactBuildingAnInterface.jpg";
-import ReactComponentsContextAndAccessibility from "../images/ReactComponentsContextAndAccessibility.jpg";
-import ReactEcosystems from "../images/ReactEcosystems.jpg";
-import ReactHooks from "../images/ReactHooks.jpg";
-import ReactTestingAndDebugging from "../images/ReactTestingAndDebugging.jpg";
-import ReactEssentialTraining from "../images/ReactEssentialTraining.jpg";
-import NodejsEssentialTraining from "../images/NodejsEssentialTraining.jpg";
-import DevelopingACriticalThinkingMindset from "../images/DevelopingACriticalThinkingMindset.jpg";
-import FullStackWebDeveloperCertificationMiamiDadeCollege from "../images/FullStackWebDeveloperCertificationMiamiDadeCollege.jpg";
-import ReactDesignPatterns from "../images/ReactDesignPatterns.jpg";
-import ReactCreatingAndHostingAFullStackSite from "../images/ReactCreatingAndHostingAFullStackSite.jpg";
+import React from 'react';
+import "../images/BecomeAReactDeveloper";
+import "../images/FirebaeEssentialTraining.jpg";
+import "../images/JavascriptCodeChallenges.jpg";
+import "../images/ReactBuildingAnInterface.jpg";
+import "../images/ReactComponentsContextAndAccessibility.jpg";
+import "../images/ReactEcosystems.jpg";
+import "../images/ReactHooks.jpg";
+import "../images/ReactTestingAndDebugging.jpg";
+import "../images/ReactEssentialTraining.jpg";
+import "../images/NodejsEssentialTraining.jpg";
+import "../images/DevelopingACriticalThinkingMindset.jpg";
+import "../images/FullStackWebDeveloperCertificationMiamiDadeCollege";
+import "../images/ReactDesignPatterns.jpg";
+import "../images/ReactCreatingAndHostingAFullStackSite.jpg";
+import {CertificationsData} from '../components/CertificationsData';
 
 
 export const CertificationShow = () => {
 	return (
 		<>
-        <img className="d-block mt-2 mb-2 w-75 h-75" src={BecomeAReactDeveloper} />
-        <img className="d-block mt-2 mb-2 w-75 h-75" src={BuildAGoogleMapAppWithJavascript} />
-        <img className="d-block mt-2 mb-2 w-75 h-75" src={FirebaeEssentialTraining} />
-        <img className="d-block mt-2 mb-2 w-75 h-75" src={JavascriptCodeChallenges} />
-        <img className="d-block mt-2 mb-2 w-75 h-75" src={ReactBuildingAnInterface} />
-        <img className="d-block mt-2 mb-2 w-75 h-75" src={ReactComponentsContextAndAccessibility} />
-        <img className="d-block mt-2 mb-2 w-75 h-75" src={ReactEcosystems} />
-        <img className="d-block mt-2 mb-2 w-75 h-75"  src={ReactHooks} />
-        <img className="d-block mt-2 mb-2 w-75 h-75"  src={ReactTestingAndDebugging} />
-        <img className="d-block mt-2 mb-2 w-75 h-75"  src={ReactEssentialTraining} />
-        <img className="d-block mt-2 mb-2 w-75 h-75"  src={NodejsEssentialTraining} />
-        <img className="d-block mt-2 mb-2 w-75 h-75"  src={DevelopingACriticalThinkingMindset} />
-        <img className="d-block mt-2 mb-2 w-75 h-75"  src={FullStackWebDeveloperCertificationMiamiDadeCollege} />
-        <img className="d-block mt-2 mb-2 w-75 h-75"  src={ReactDesignPatterns} />
-        <img className="d-block mt-2 mb-2 w-75 h-75"  src={ReactCreatingAndHostingAFullStackSite} />
+        {CertificationsData.map((item)=>{
+            return (
+                <div>
+                    <img variant="top" className="d-block mt-2 mb-2 w-75 h-75" src={`${item.img}`} key={item.id} />
+                </div>
+                
+                )
+        })}
         </>
     )
-}
+};
