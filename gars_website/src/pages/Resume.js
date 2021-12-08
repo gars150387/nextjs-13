1d7c16e104a1ef77044e32a5a141c64af06ed52e
Gustavo <gar.santeliz@gmail.com>
2021-12-07 20:07:03 -0500

    structuring resume component with pdf component to render pdf file

--- gars_website/src/pages/Resume.js
+++ gars_website/src/pages/Resume.js
@@ -1,7 +1,15 @@
+import React from "react";
+import SinglePagePDFViewer from "./components/pdf/single-page";
 
-// import GustavoRodriguezSanteliz_SoftwareDeveloper from '../images/GustavoRodriguezSanteliz_SoftwareDeveloper';
-// export const Resume = ()=>{
-//     return (
-//         // <img src={GustavoRodriguezSanteliz_SoftwareDeveloper} />
-//     )
-// }
+/* This is required only if the project file is located 
+inside the app. Otherwise you can use the external link of the pdf file*/
+import GustavoRodriguezSanteliz_SoftwareDeveloper from "../pdf/GustavoRodriguezSanteliz_SoftwareDeveloper.PDF";
+
+export const Resume = ()=> {
+  return (
+    <div className="App">
+      <h4>Single Page</h4>
+      <SinglePagePDFViewer pdf={GustavoRodriguezSanteliz_SoftwareDeveloper} />
+    </div>
+  );
+}
