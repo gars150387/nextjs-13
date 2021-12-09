f7fd68cdd4df4bd9b3e96ce8ac5e4dc8ec68c995
Gustavo <gar.santeliz@gmail.com>
2021-12-08 20:00:12 -0500

    adding more certification

--- gars_website/src/components/CertificationShow.js
+++ gars_website/src/components/CertificationShow.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import BecomeAReactDeveloper from "../images/BecomeAReactDeveloper";
-import FirebaeEssentialTraining from "../images/FirebaeEssentialTraining.jpg";
-import JavascriptCodeChallenges from "../images/JavascriptCodeChallenges.jpg";
-import ReactBuildingAnInterface from "../images/ReactBuildingAnInterface.jpg";
-import ReactComponentsContextAndAccessibility from "../images/ReactComponentsContextAndAccessibility.jpg";
-import ReactEcosystems from "../images/ReactEcosystems.jpg";
+// import React from 'react';
+// import BecomeAReactDeveloper from "../images/BecomeAReactDeveloper";
+// import FirebaeEssentialTraining from "../images/FirebaeEssentialTraining.jpg";
+// import JavascriptCodeChallenges from "../images/JavascriptCodeChallenges.jpg";
+// import ReactBuildingAnInterface from "../images/ReactBuildingAnInterface.jpg";
+// import ReactComponentsContextAndAccessibility from "../images/ReactComponentsContextAndAccessibility.jpg";
+// import ReactEcosystems from "../images/ReactEcosystems.jpg";
 // import "../images/ReactHooks.jpg";
 // import "../images/ReactTestingAndDebugging.jpg";
 // import "../images/ReactEssentialTraining.jpg";
@@ -13,20 +13,20 @@ import ReactEcosystems from "../images/ReactEcosystems.jpg";
 // import "../images/FullStackWebDeveloperCertificationMiamiDadeCollege";
 // import "../images/ReactDesignPatterns.jpg";
 // import "../images/ReactCreatingAndHostingAFullStackSite.jpg";
-import {CertificationsData} from '../components/CertificationsData';
+// import {CertificationsData} from '../components/CertificationsData';
 
 
-export const CertificationShow = () => {
-	return (
-		<>
-        {CertificationsData.map((item)=>{
-            return (
-                <div>
-                    <img variant="top" className="d-block mt-2 mb-2 w-75 h-75" src={item.img} key={item.id} />
-                </div>
+// export const CertificationShow = () => {
+// 	return (
+// 		<>
+//         {CertificationsData.map((item)=>{
+//             return (
+//                 <div>
+//                     <img variant="top" className="d-block mt-2 mb-2 w-75 h-75" src={item.img} key={item.id} />
+//                 </div>
                 
-                )
-        })}
-        </>
-    )
-};
+//                 )
+//         })}
+//         </>
+//     )
+// };
