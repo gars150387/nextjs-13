4db37f9a0c9bd7675e2ea006c84e5f5981046bdd
Gustavo <gar.santeliz@gmail.com>
2021-12-08 19:32:59 -0500

    create a component to structure the code

---
+++ gars_website/src/components/ResumeFile.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Viewer } from '@react-pdf-viewer/core'
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
+import "@react-pdf-viewer/core/lib/styles/index.className"
+import '@react-pdf-viewer/default-layout/lib/styles.index.css'
+
+import { Worker } from '@react-pdf-viewer/core'
+
+import { isPdfFile } from '../pdf/GustavoRodriguezSanteliz_SoftwareDeveloper.PDF'
+
+export const ResumeFile = () =>{
+    return (
+        <div>
+            <form>
+                ...
+            </form>
+        </div>
+    )
+}
