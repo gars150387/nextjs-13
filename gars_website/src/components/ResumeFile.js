9680fba93069d5ea1d365f893dc66f143be91e3a
Gustavo <gar.santeliz@gmail.com>
2021-12-08 19:47:50 -0500

    viewer code structured

--- gars_website/src/components/ResumeFile.js
+++ gars_website/src/components/ResumeFile.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Viewer } from '@react-pdf-viewer/core'
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
-import "@react-pdf-viewer/core/lib/styles/index.className"
-import '@react-pdf-viewer/default-layout/lib/styles.index.css'
+import "@react-pdf-viewer/core/lib"
+import '@react-pdf-viewer/default-layout'
 
 import { Worker } from '@react-pdf-viewer/core'
 
-import { isPdfFile } from '../pdf/GustavoRodriguezSanteliz_SoftwareDeveloper.PDF'
+import PdfFile from '../pdf/GustavoRodriguezSanteliz_SoftwareDeveloper.PDF'
 
 export const ResumeFile = () =>{
+
+    const [defaultPdfFile] = useState(PdfFile);
+
+    // const defaultLayoutPluginInstance = defaultLayoutPlugin()
+
     return (
-        <div>
-            <form>
-                ...
-            </form>
+        <div className="pdf-container">
+            {defaultPdfFile && <> <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
+            <Viewer fileUrl={defaultPdfFile}
+        // plugins={[defaultLayoutPluginInstance]}
+        
+        />
+        </Worker></>}
         </div>
     )
 }
