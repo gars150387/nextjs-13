acf431c76b794c1db761ac2a3ac1412d52530975
Gustavo <gar.santeliz@gmail.com>
2021-12-09 19:50:47 -0500

    redesinging components to better reading

--- gars_website/src/components/CertificationShow.js
+++ gars_website/src/components/CertificationShow.js
@@ -1,32 +1,38 @@
-// import React from 'react';
-// import BecomeAReactDeveloper from "../images/BecomeAReactDeveloper";
-// import FirebaeEssentialTraining from "../images/FirebaeEssentialTraining.jpg";
-// import JavascriptCodeChallenges from "../images/JavascriptCodeChallenges.jpg";
-// import ReactBuildingAnInterface from "../images/ReactBuildingAnInterface.jpg";
-// import ReactComponentsContextAndAccessibility from "../images/ReactComponentsContextAndAccessibility.jpg";
-// import ReactEcosystems from "../images/ReactEcosystems.jpg";
-// import "../images/ReactHooks.jpg";
-// import "../images/ReactTestingAndDebugging.jpg";
-// import "../images/ReactEssentialTraining.jpg";
-// import "../images/NodejsEssentialTraining.jpg";
-// import "../images/DevelopingACriticalThinkingMindset.jpg";
-// import "../images/FullStackWebDeveloperCertificationMiamiDadeCollege";
-// import "../images/ReactDesignPatterns.jpg";
-// import "../images/ReactCreatingAndHostingAFullStackSite.jpg";
-// import {CertificationsData} from '../components/CertificationsData';
+import React from 'react';
+import "../images/BecomeAReactDeveloper.jpg";
+import "../images/FirebaeEssentialTraining.jpg";
+import "../images/JavascriptCodeChallenges.jpg";
+import "../images/ReactBuildingAnInterface.jpg";
+import "../images/ReactComponentsContextAndAccessibility.jpg";
+import "../images/ReactEcosystems.jpg";
+import "../images/ReactHooks.jpg";
+import "../images/ReactTestingAndDebugging.jpg";
+import "../images/ReactjsEssentialTraining.jpg";
+import "../images/NodejsEssentialTraining.jpg";
+import "../images/DevelopingACriticalThinkingMindset.jpg";
+import "../images/FullStackWebDeveloperCertificationMiamiDadeCollege.JPG";
+import "../images/ReactDesignPatterns.JPG";
+import "../images/ReactCreatingAndHostingAFullStackSite.JPG";
+import "../images/AcceleratedJavaScriptTrainingUdemy.jpg"
 
-
-// export const CertificationShow = () => {
-// 	return (
-// 		<>
-//         {CertificationsData.map((item)=>{
-//             return (
-//                 <div>
-//                     <img variant="top" className="d-block mt-2 mb-2 w-75 h-75" src={item.img} key={item.id} />
-//                 </div>
-                
-//                 )
-//         })}
-//         </>
-//     )
-// };
+export const CertificationShow = () => {
+    return (
+        <>
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/FullStackWebDeveloperCertificationMiamiDadeCollege.JPG").default} alt="Full Stack Web Developer Certification from Miami Dade College"/>
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/AcceleratedJavaScriptTrainingUdemy.jpg").default} alt="Accelerated Javascript Training Udemy"/>
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/BecomeAReactDeveloper.jpg").default} alt="Become A React Developer" />
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/ReactjsEssentialTraining.jpg").default} alt="React essential training"/>
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/ReactHooks.jpg").default} alt="React Hooks" />
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/ReactComponentsContextAndAccessibility.jpg").default} alt="React components context and accessability"/>
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/ReactTestingAndDebugging.jpg").default} alt="React Testing and debugging"/>
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/ReactEcosystems.jpg").default} alt="React Ecosystems" />
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/ReactDesignPatterns.JPG").default} alt="React design patterns"/>
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/NodejsEssentialTraining.jpg").default} alt="Node essential training"/>
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/FirebaeEssentialTraining.jpg").default} alt="Firebase Essential Training" />
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/JavascriptCodeChallenges.jpg").default} alt="Javascript Challenges"/>
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/ReactBuildingAnInterface.jpg").default} alt="React Building an interface"/>
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/DevelopingACriticalThinkingMindset.jpg").default} alt="Developing a critical mindset"/>
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/ReactCreatingAndHostingAFullStackSite.JPG").default} alt="React creating and hosting a full stack site"/>
+        </>
+    );
+}
