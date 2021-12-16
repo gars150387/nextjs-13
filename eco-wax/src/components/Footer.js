d7f6a5b01265ea6670bc5fc79d75dbcfb5ce5068
Gustavo <gar.santeliz@gmail.com>
2021-12-15 21:58:32 -0500

    added icons

--- eco-wax/src/components/Footer.js
+++ eco-wax/src/components/Footer.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import {AiOutlineCopyrightCircle} from 'react-icons/ai'
+import { AiOutlineCopyrightCircle } from 'react-icons/ai'
+import { MdOutlineHighQuality, MdAttachMoney } from 'react-icons/md'
+import { FaShippingFast } from 'react-icons/fa'
 
 export const Footer = () => {
     return (
         <div className='footer container-fluid'>
-            <div>
-                <img></img>
+            <div className='footer-icons'>
+                <MdOutlineHighQuality style={{ color: "white" }} />
+                <FaShippingFast style={{ color: "white" }} />
+                <MdAttachMoney style={{ color: "white" }} />
             </div>
             <div>
-                <div>
-                </div>
-            </div>
-            <div>
-                <span>Copyright <AiOutlineCopyrightCircle/> 2021 Eco Wax | Designed by G.A.R.S</span>
+                <span>Copyright <AiOutlineCopyrightCircle /> 2021 Eco Wax | Designed by G.A.R.S</span>
             </div>
         </div>
     )
