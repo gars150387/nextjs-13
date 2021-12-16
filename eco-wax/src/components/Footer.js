f36d0c69d22dd8b42f5e1700d4987e29934b58b4
Gustavo <gar.santeliz@gmail.com>
2021-12-15 21:47:57 -0500

    structuring footer

---
+++ eco-wax/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {AiOutlineCopyrightCircle} from 'react-icons/ai'
+
+export const Footer = () => {
+    return (
+        <div className='footer container-fluid'>
+            <div>
+                <img></img>
+            </div>
+            <div>
+                <div>
+                </div>
+            </div>
+            <div>
+                <span>Copyright <AiOutlineCopyrightCircle/> 2021 Eco Wax | Designed by G.A.R.S</span>
+            </div>
+        </div>
+    )
+}
