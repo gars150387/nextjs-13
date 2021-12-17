bb15f0b53f8ae08a6ea052e03d08cb59666ba853
Gustavo <gar.santeliz@gmail.com>
2021-12-16 20:32:20 -0500

    re structuring app order

--- eco-wax/src/App.js
+++ eco-wax/src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
-import { Footer } from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Catalogo } from './pages/Catalogo';
+import {Router, Route, Switch} from 'react-dom-route'
+import {Footer} from '../components/footer'
+import {NavbarMain} from '../components/navbar'
+
 
 function App() {
+
   return (
-    <div className="App">
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <NavbarMain sticky="top"/>
+      </div>
+      <Switch>
+        <Route exact path="/home" >
+          <Home />
+        </Route>
+        <Route exact path="/catalogo" >
+          <Catalogo/>
+        </Route>
+        <Route>
+          404 Page not found.
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
