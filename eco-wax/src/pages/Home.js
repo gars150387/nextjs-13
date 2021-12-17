2b3c55551833bf61030688e6a5c22fbcc78019f9
Gustavo <gar.santeliz@gmail.com>
2021-12-16 20:17:06 -0500

    added component to home

---
+++ eco-wax/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CarouselMain } from '../components/Carousel'
+import { CardContent } from '../components/Content'
+
+
+export const Home = () => {
+    return (
+        <>
+            <CarouselMain />
+            <CardContent />
+        </>
+    )
+}
