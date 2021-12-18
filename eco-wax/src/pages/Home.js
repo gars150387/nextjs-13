ec41e60b0c9a76574e073c97cf60d66bf9f41f0c
Gustavo <gar.santeliz@gmail.com>
2021-12-18 18:37:57 -0500

    re structuring home component

--- eco-wax/src/pages/Home.js
+++ eco-wax/src/pages/Home.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { CarouselMain } from '../components/Carousel'
 import { CardContent } from '../components/Content'
+import { Inicio } from '../components/Inicio'
+import { ModalTrigger } from '../components/ModalTrigger'
 
 
 export const Home = () => {
     return (
         <>
-            <CarouselMain />
-            <CardContent />
+            <Inicio />
         </>
     )
 }
