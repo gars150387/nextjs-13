fcaa910422736d4c3553d77d8f1e969294e6290d
Gustavo <gar.santeliz@gmail.com>
2021-12-18 22:51:51 -0500

    set up sttributes

--- eco-wax/src/components/Footer.js
+++ eco-wax/src/components/Footer.js
@@ -6,12 +6,12 @@ import { FaShippingFast } from 'react-icons/fa'
 export const Footer = () => {
     return (
         <div className='footer container-fluid'>
-            <div className='footer-icons'>
-                <MdOutlineHighQuality style={{ color: "white" }} />
-                <FaShippingFast style={{ color: "white" }} />
-                <MdAttachMoney style={{ color: "white" }} />
+            <div className='footer-icons text-end'>
+                <MdOutlineHighQuality style={{ color: "white", marginRight: "8px", fontSize: "35px" }} />
+                <FaShippingFast style={{ color: "white", marginRight: "2px", fontSize: "35px"  }} />
+                <MdAttachMoney style={{ color: "white", fontSize: "35px"  }} />
             </div>
-            <div>
+            <div className='text-start' style={{position: "relative", bottom: "-100px"}}>
                 <span>Copyright <AiOutlineCopyrightCircle /> 2021 Eco Wax | Designed by G.A.R.S</span>
             </div>
         </div>
