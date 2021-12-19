31682d7805b3dfd2c5bce5c5a40cacd4e1b19dd7
Gustavo <gar.santeliz@gmail.com>
2021-12-18 19:15:09 -0500

    trying to add video

---
+++ eco-wax/src/components/Video1.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ReactPlayer } from 'video-react';
+
+
+export const VideoPlayer = () => {
+  return (
+    <ReactPlayer
+      src="https://youtu.be/Nl54MJDR2p8"/>
+  );
+};
