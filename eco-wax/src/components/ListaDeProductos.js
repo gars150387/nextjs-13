21cf88e86a6047f8714082bbf1ffaf02a948704b
Gustavo <gar.santeliz@gmail.com>
2021-12-19 00:17:59 -0500

    list of product created

---
+++ eco-wax/src/components/ListaDeProductos.js
@@ -0,0 +1,33 @@
+export const ListaDeProductos= [{
+    id: "001",
+    nombre: "Lavado Ecologico",
+    caracteristica: ['Ahorra agua', ' Rinde de 6 a 7 lavadas', ' Porcelaniza la pintura', ' Limpia y desempana cristales' , ' Funciona en cualquier tipo de pintura'],
+    presentacion: '500 ml',
+    precio: "$38.900",
+    obsequio: "2 Microfibra",
+    img: "pic1"
+},{
+    id: "002",
+    nombre: "Lavado Ecologico - Motos",
+    caracteristica: ['Ahorra agua', ' Rinde de 6 a 7 lavadas', ' Porcelaniza la pintura', ' Desempanante' , ' Recupera la farola', ' Funciona en pintura mata'],
+    presentacion: '250 ml',
+    precio: "$24.900",
+    obsequio: "1 Microfibra",
+    img: "pic2"
+},{
+    id: "003",
+    nombre: "Restaurador de Partes Negras",
+    caracteristica: ['Restaura el color original de las piezas de tu carra y moto', 'Brillo y proteccion', 'No deja sensacion grasosa', 'Funciona para partes internas y externas' , 'Hidrata cojineria de cuero', 'Fragancia de coco'],
+    presentacion: '300 ml',
+    precio: "$22.900",
+    obsequio: "Una esponjilla",
+    img: "pic3"
+},{
+    id: "004",
+    nombre: "Panos de Microfibra",
+    caracteristica: ['Larga duracion', 'Ultra absorbentes', '80% Poliester, 20% Poliamida' , '40,6 cm x 40,6 cm', 'Para uso del interior y exterior de tu carro', 'Producto importado de alta calidad'],
+    presentacion: 'Paquete de 2 Unidades',
+    precio: "$11.900",
+    obsequio: null,
+    img: "pic4"
+}]
