cc6c4b6396f19c746c3d80d212ccf07dc040cb7a
Gustavo <gar.santeliz@gmail.com>
2021-12-19 17:16:27 -0500

    trying to render features individually

--- eco-wax/src/components/Producto.js
+++ eco-wax/src/components/Producto.js
@@ -8,21 +8,22 @@ import { Card, Button } from 'react-bootstrap'
 // import '../images/pic4/jpeg'
 
 export const Producto = () => {
+    let index = 0
     return (
         <>
             {ListaDeProductos.map((item) => {
                 return (
                     <Card key={item.id} style={{ width: "43%", borderRadius: "10px", margin: "auto", marginTop: "20px" }}>
-                        {/* <Card.Img variant="top" src={`${item.img}`} alt={`${item.img}`} /> */}
+                        {/* <Card.Img variant="top" src={`./${item.img}.jpeg`} /> */}
                         <Card.Body>
-                            <Card.Title><h2 className='bg-dark w-100' style={{height: "75px", color: "white", position: "relative"}}>{item.nombre}</h2></Card.Title>
+                            <Card.Title><h2 className='bg-dark w-100' style={{ height: "75px", color: "white", position: "relative" }}>{item.nombre}</h2></Card.Title>
                             <Card.Text>
                             </Card.Text>
                             <h4>
                                 Descripcion:
                             </h4>
                             <p>
-                                {item.caracteristica}
+                                {item.caracteristica > index && <div>{item.caracteristica[index]}</div>}
                             </p>
                             <h5>
                                 {item.presentacion}
@@ -36,7 +37,7 @@ export const Producto = () => {
                                     Precio: {item.precio}
                                 </h4>
                             </Card.Text>
-                            <Button variant="success">Compralo Ahora!</Button>
+                            <Button variant="success">Agregar al Carrito</Button>
                         </Card.Body>
                     </Card>
                 )
