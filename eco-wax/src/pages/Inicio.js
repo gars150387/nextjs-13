78b24cd8d00628724271745811fb629cd40d3f7a
Gustavo <gar.santeliz@gmail.com>
2021-12-18 21:38:22 -0500

    set up attributes

--- eco-wax/src/pages/Inicio.js
+++ eco-wax/src/pages/Inicio.js
@@ -7,7 +7,7 @@ export const Inicio = () => {
     return (
         <div>
             {/* <NavbarMain /> */}
-            <img className="w-100 h-100" src="https://media0.giphy.com/media/FMsbSSjEe0N6zH7K7x/giphy.gif?cid=790b7611d71857a984c6a5e0cd02a27215263df12952d22f&rid=giphy.gif&ct=g"
+            <img className="w-100" src="https://media0.giphy.com/media/FMsbSSjEe0N6zH7K7x/giphy.gif?cid=790b7611d71857a984c6a5e0cd02a27215263df12952d22f&rid=giphy.gif&ct=g"
                 allowFullScreen>
             </img>
             <ModalTrigger />
