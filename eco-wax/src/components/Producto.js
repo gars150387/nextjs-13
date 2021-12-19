302167ae587eabc06dc81b8f60a8674262af7736
Gustavo <gar.santeliz@gmail.com>
2021-12-19 01:06:42 -0500

    set up style

---
+++ eco-wax/src/components/Producto.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { ListaDeProductos } from './ListaDeProductos'
+import { Card, Button } from 'react-bootstrap'
+// import {Caracteristicas} from './ListaDeCaracteristicas'
+// import '../images/pic1/jpeg'
+// import '../images/pic2/jpeg'
+// import '../images/pic3/jpeg'
+// import '../images/pic4/jpeg'
+
+export const Producto = () => {
+    return (
+        <>
+            {ListaDeProductos.map((item) => {
+                return (
+                    <Card key={item.id} style={{ width: "43%", borderRadius: "10px", margin: "auto", marginTop: "20px" }}>
+                        {/* <Card.Img variant="top" src={`${item.img}`} alt={`${item.img}`} /> */}
+                        <Card.Body>
+                            <Card.Title><h2 className='bg-dark w-100' style={{height: "75px", color: "white", position: "relative"}}>{item.nombre}</h2></Card.Title>
+                            <Card.Text>
+                            </Card.Text>
+                            <h4>
+                                Descripcion:
+                            </h4>
+                            <p>
+                                {item.caracteristica}
+                            </p>
+                            <h5>
+                                {item.presentacion}
+                            </h5>
+                            <Card.Text>
+                                <h4>
+                                    Obsequio:
+                                </h4>
+                                <p>{item.obsequio}</p>
+                                <h4>
+                                    Precio: {item.precio}
+                                </h4>
+                            </Card.Text>
+                            <Button variant="success">Compralo Ahora!</Button>
+                        </Card.Body>
+                    </Card>
+                )
+            })}
+        </>
+    )
+}
