c1d70d6894574045c4dbc00523a99b97807a508c
Gustavo <gar.santeliz@gmail.com>
2021-12-21 19:25:13 -0500

    renaming imported component

--- eco-wax/src/pages/ContactUs.js
+++ eco-wax/src/pages/ContactUs.js
@@ -1,11 +1,11 @@
-import { ContactUs } from "../components/ContactForm1";
+import { Contact } from "../components/Contact";
 
-export const Contact = () => {
+export const ContactUs = () => {
     return (
         <>
         <h3 className="text-center mt-4">Contact Me</h3>
         <p className="text-center">If you have any kind of question, please feel free to reach out to me, I am more than glad to help you with your concern. <br/><span className="color-back">Thank you</span></p>
-            <ContactUs className="contactForm" />
+            <Contact className="contactForm" />
         </>
     )
 }
