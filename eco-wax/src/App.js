a7110a5732e8f55fda44695b32f1ad94f2ca7078
Gustavo <gar.santeliz@gmail.com>
2021-12-21 19:25:50 -0500

    importing component and routing it

--- eco-wax/src/App.js
+++ eco-wax/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Catalogo } from './pages/Catalogo'
 import { Inicio } from './pages/Inicio'
+import { ContactUs } from './pages/ContactUs';
 
 
 function App() {
@@ -12,9 +13,10 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path='/' element={<Inicio /> } />
-          <Route path='/inicio' element={<Home />} />
+          <Route exact path='/' element={<Inicio /> } />
+          <Route exact path='/inicio' element={<Home />} />
           <Route extac path='/catalogo' element={<Catalogo />} />
+          <Route exact path='/contact' element={<ContactUs/>} />
           <Route>
             404 Page not found.
           </Route>
