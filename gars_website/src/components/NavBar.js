3757122677e117f99f8c8354782b6e26432636f4
Gustavo <gar.santeliz@gmail.com>
2021-12-22 19:56:16 -0500

    change position of options in navbar

--- gars_website/src/components/NavBar.js
+++ gars_website/src/components/NavBar.js
@@ -11,32 +11,27 @@ export const NavBarMain = () => {
         <Link to="/home">
           <Navbar.Brand sticky="top" className="text-start sticky-top" href="#">G.A.R.S</Navbar.Brand>
         </Link>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" className="sticky-top"/>
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav sticky="top" className="text-end sticky-top">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" className="sticky-top" />
+        <Navbar.Collapse id="responsive-navbar-nav" className="text-end">
+          <Nav sticky="top" className="justify-content-end sticky-top">
             <Link to="/certifications">
               <Nav.Link href="/certifications">Certifications</Nav.Link>
+            </Link>
+            <Link to="/resume">
+              <Nav.Link className="text-end" href="/resume">Resume</Nav.Link>
+            </Link>
+            <Link to="/contact">
+              <Nav.Link className="text-end" eventKey={2} href="/contact">Contact</Nav.Link>
             </Link>
             <NavDropdown title="Projects" id="collasible-nav-dropdown">
               <NavDropdown.Item href="https://github.com/gars150387/Contact_list_Flux_API_BasedOnArticle">Contact_list_Flux_API_BasedOnArticle</NavDropdown.Item>
               <NavDropdown.Item href="https://github.com/WoodlyB/Gustavo-and-Woodly-project">Final Project</NavDropdown.Item>
-              <NavDropdown.Item href="https://github.com/gars150387/todo-list-in-landing-page-react-flux">To Do List</NavDropdown.Item>              
+              <NavDropdown.Item href="https://github.com/gars150387/todo-list-in-landing-page-react-flux">To Do List</NavDropdown.Item>
               <NavDropdown.Item href="https://github.com/gars150387/simple-counter-exercise-react-hook">Counter</NavDropdown.Item>
               <NavDropdown.Item href="https://github.com/gars150387/traffic-light-exercise-react">Traffic Light</NavDropdown.Item>
               <NavDropdown.Item href="https://github.com/gars150387/random-card-generated">Random Card generated</NavDropdown.Item>
               <NavDropdown.Item href="https://github.com/gars150387/random-excuse-exercise-javascript">Random Excuse Generated</NavDropdown.Item>
-              
-              {/* <NavDropdown.Divider />
-              <NavDropdown.Item href="#/3.4">Separated link</NavDropdown.Item> */}
             </NavDropdown>
-          </Nav>
-          <Nav sticky="top">
-            <Link to="/resume">
-              <Nav.Link className="text-end" href="/resume">Resume</Nav.Link>
-            </Link>
-            <Link to="/contact">            
-            <Nav.Link className="text-end" eventKey={2} href="/contact">Contact</Nav.Link>
-            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
