464fcc6be5dca67f4051176e632a858c3bfe4e83
Gustavo <gar.santeliz@gmail.com>
2021-12-23 19:07:41 -0500

    search component imported

---
+++ giphy-api-exercise/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Search } from './component/Search';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Super Awesome Weather App</h1>
+      <Search/>
+    </div>
+  );
+}
+
+export default App;
