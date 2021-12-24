4d962a000704bdbee715cb22ae492eb4bdc3c2ad
Gustavo <gar.santeliz@gmail.com>
2021-12-23 19:10:48 -0500

    component to show result created

---
+++ giphy-api-exercise/src/component/Weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Weather = () =>{
+    return (
+        <div className="weather">
+            <h1 id="weather">City Name</h1>
+            <div id="weatherCity">Weather Description</div>
+            <div id="weatherTempurate">Weather Temperute</div>
+        </div>
+    )
+}
