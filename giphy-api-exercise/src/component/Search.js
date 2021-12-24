90d6d79aa7fc7fc2fa73db9efccc5c7539c28654
Gustavo <gar.santeliz@gmail.com>
2021-12-23 19:54:00 -0500

    structuring function

--- giphy-api-exercise/src/component/Search.js
+++ giphy-api-exercise/src/component/Search.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const Search = () =>{
+    const [update, setUpdate] = useState('')
+    let searchCity = setUpdate
+    {searchCity.lenght < 1 && alert("Please enter a valid city") }
+    let http = new XMLHttpRequest()
+    let apiKey = "750f844a271e6a0e4d5caf0107a40189"
+    let url = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={750f844a271e6a0e4d5caf0107a40189}'
+    let method = 'GET'
+
+    http.open(method, url)
+    http.onreadystatechange = function(){
+        if (http.readyState === XMLHttpRequest.DONE && http.status === 200){
+
+        }else if (http.readyState === XMLHttpRequest.DONE){
+            alert("Somethign went wrong!")
+        }
+    }
+
     return (
         <div className="main">
-            <label for="city">City Name</label>
-            <input type='text' id='city'></input>
-            <button type="button">Show Weather</button>
+            <label>City Name</label>
+            <input placeholder="City name" type='text' id='city' onChange={element => setUpdate(element. target.value)}></input>
+            {}
+            <button type="button" onClick={function(){
+                console.log("clicked")
+            }
+            }>Show Weather</button>
         </div>
     )
 }
