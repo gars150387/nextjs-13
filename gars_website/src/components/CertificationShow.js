cbffeddb832724b277ecbf27f2f0fe0079fb8e41
Gustavo <gar.santeliz@gmail.com>
2021-12-30 18:23:56 -0500

    organizing certificates

--- gars_website/src/components/CertificationShow.js
+++ gars_website/src/components/CertificationShow.js
@@ -11,10 +11,13 @@ import "../images/ReactjsEssentialTraining.jpg";
 import "../images/NodejsEssentialTraining.jpg";
 import "../images/DevelopingACriticalThinkingMindset.jpg";
 import "../images/FullStackWebDeveloperCertificationMiamiDadeCollege.JPG";
-import "../images/ReactDesignPatterns.JPG";
+import "../images/ReactDesignPatterns.png";
 import "../images/ReactCreatingAndHostingAFullStackSite.JPG";
 import "../images/AcceleratedJavaScriptTrainingUdemy.jpg"
 import "../images/IntroductionToWebAPIs.png"
+import "../images/GitEssentialTrainingTheBasics.png"
+import "../images/JSONEssentialTraining.png"
+import "../images/ReactWorkingWithAPIs.png"
 
 export const CertificationShow = () => {
     return (
@@ -27,14 +30,17 @@ export const CertificationShow = () => {
             <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/ReactComponentsContextAndAccessibility.jpg").default} alt="React components context and accessability"/>
             <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/ReactTestingAndDebugging.jpg").default} alt="React Testing and debugging"/>
             <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/ReactEcosystems.jpg").default} alt="React Ecosystems" />
-            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/ReactDesignPatterns.JPG").default} alt="React design patterns"/>
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/ReactDesignPatterns.png").default} alt="React design patterns"/>
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/ReactBuildingAnInterface.jpg").default} alt="React Building an interface"/>
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/ReactCreatingAndHostingAFullStackSite.JPG").default} alt="React creating and hosting a full stack site"/>
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/ReactWorkingWithAPIs.png").default} alt="Introduction to Web API"/>
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/GitEssentialTrainingTheBasics.png").default} alt="Introduction to Web API"/>
             <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/NodejsEssentialTraining.jpg").default} alt="Node essential training"/>
             <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/FirebaeEssentialTraining.jpg").default} alt="Firebase Essential Training" />
-            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/JavascriptCodeChallenges.jpg").default} alt="Javascript Challenges"/>
-            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/ReactBuildingAnInterface.jpg").default} alt="React Building an interface"/>
-            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/DevelopingACriticalThinkingMindset.jpg").default} alt="Developing a critical mindset"/>
-            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/ReactCreatingAndHostingAFullStackSite.JPG").default} alt="React creating and hosting a full stack site"/>
             <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/IntroductionToWebAPIs.png").default} alt="Introduction to Web API"/>
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/JSONEssentialTraining.png").default} alt="Introduction to Web API"/>
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/JavascriptCodeChallenges.jpg").default} alt="Javascript Challenges"/>
+            <img variant="top" id="certificationsImage" className="d-block mt-2 mb-2 w-75 h-75" src={require("../images/DevelopingACriticalThinkingMindset.jpg").default} alt="Developing a critical mindset"/>
         </>
     );
 }
