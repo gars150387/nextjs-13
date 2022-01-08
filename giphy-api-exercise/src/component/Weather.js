145185a4fdcd3735f3486dee9ff72be126967829
Gustavo <gar.santeliz@gmail.com>
2022-01-07 23:21:11 -0500

    add props

--- giphy-api-exercise/src/component/Weather.js
+++ giphy-api-exercise/src/component/Weather.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-export const Weather = () =>{
+export const Weather = (props) =>{
     return (
         <div className="weather">
-            <h1 id="weather">City Name</h1>
-            <div id="weatherCity">Weather Description</div>
-            <div id="weatherTempurate">Weather Temperute</div>
+            <h1 id="weather">City Name: {props.city}</h1>
+            <div id="weatherCity">Weather Description: {props.weather}</div>
+            <div id="weatherTempurate">Weather Temperute: {props.main}</div>
         </div>
     )
 }
