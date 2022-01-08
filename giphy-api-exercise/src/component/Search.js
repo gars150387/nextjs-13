28c438cb1bc42a53880a85592a9bbd3b25a8b3a4
Gustavo <gar.santeliz@gmail.com>
2022-01-07 23:20:43 -0500

    structuring code to render data

--- giphy-api-exercise/src/component/Search.js
+++ giphy-api-exercise/src/component/Search.js
@@ -1,32 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export const SearchWeather = () => {
-    const [update, setUpdate] = useState('')
-    // let cityName = setUpdate.value
-    // // let http = new XMLHttpRequest()
-    // let apiKey = "750f844a271e6a0e4d5caf0107a40189"
-    // let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=' + apiKey;
-    // let method = 'GET'
+    const [setUpdate] = useState('')
 
-    // http.open(method, url)
-    // http.onreadystatechange = function () {
-    //     if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
-    //         let data = JSON.parse(http.responseText);
-    //         let weatherData = new Weather(cityName, data.weather[0].description.toUpperCase());
-    //         weatherData.temperate = data.main.temp;
-    //         console.log(weatherData)
-    //     } else if (http.readyState === XMLHttpRequest.DONE) {
-    //         alert("Somethign went wrong!")
-    //     }
-    //     http.send()
-    // }
+    const [data, setData] = useState([]);
+    let cityName = setUpdate
 
+
+    let apiKey = '750f844a271e6a0e4d5caf0107a40189'
+
+    let url = 'api.openweathermap.org/data/2.5/weather?q=london&appid=' + { apiKey };
+
+    useEffect(() => {
+        fetch(url, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+            .then((response) => {
+                return response.json()
+            })
+            .then(data => {
+                setData(
+                    JSON.stringify(setData),
+                    console.log('data', data),
+                    console.log('setData', setData)    
+                )
+            })
+    })
     return (
         <div className="main">
             <label>City Name</label>
             <input placeholder="City name" type='text' id='city' onChange={element => setUpdate(element.target.value)} />
             <button type="button" onClick={() => {
             }}>Show Weather</button>
+            
         </div>
     )
 }
