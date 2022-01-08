675dfc5c5f23d94ff40e9853706b0670e9df5235
Gustavo <gar.santeliz@gmail.com>
2022-01-07 23:21:35 -0500

    cleaning up component

--- giphy-api-exercise/src/App.js
+++ giphy-api-exercise/src/App.js
@@ -1,13 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import { Search } from './component/Search';
+import { SearchWeather } from './component/Search';
 import { Weather } from './component/Weather';
 
 function App() {
+
+  // const [data, setData] = useState([]);
+
+  // let apiKey = '750f844a271e6a0e4d5caf0107a40189'
+
+  // let url = 'api.openweathermap.org/data/2.5/weather?q='+{cityName}+'&appid='+{apiKey};
+
+  // useEffect(()=>{
+  //   fetch(url)
+  //   .then((response)=>{
+  //     return response.json()
+  //   })
+  //   .then((setData) =>{
+  //     JSON.stringify(setData)
+  //     console.log('data', data)
+  //     console.log('setData', setData)
+  //   })
+  // })
   return (
     <div className="App">
       <h1>The Super Awesome <br/> Weather App</h1>
-      <Search />
+      <SearchWeather />
       <Weather />
     </div>
   );
