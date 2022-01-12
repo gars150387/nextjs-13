0ca3a20cfb24ad8ba6b7f954da1035d7dd921c2c
Gustavo <gar.santeliz@gmail.com>
2022-01-12 18:14:01 -0500

    creting a function to render fetch data

--- giphy-api-exercise/src/component/Search.js
+++ giphy-api-exercise/src/component/Search.js
@@ -9,23 +9,25 @@ export const SearchWeather = () => {
 
     let url = 'api.openweathermap.org/data/2.5/weather?q='+{cityName}+'&appid=750f844a271e6a0e4d5caf0107a40189';
 
-    useEffect(() => {
-        fetch(url)
-            .then((response) => {
-                return response.json()
-                console.log(response)
-            })
-            .then( response => setData => {
-                console.log('setData', setData)
-            })
+    const Fetching =() =>{
+        useEffect(() => {
+            fetch(url)
+                .then((response) => {
+                    return response.json()
+                    console.log(response)
+                })
+                .then( response => setData => {
+                    console.log('setData', setData)
+                })
+    
+        })    
+    }
 
-    })
     return (
         <div className="main">
             <label>City Name</label>
             <input placeholder="City name" type='text' id='city' onChange={element => setUpdate(element.target.value)} />
-            <button type="button" onClick={() => {
-            }}>Show Weather</button>
+            <button type="button" onClick={() => Fetching() }>Show Weather</button>
 
         </div>
     )
