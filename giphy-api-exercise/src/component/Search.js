5561aea2116715a1c61dea2a4e7326cb2cad4198
Gustavo <gar.santeliz@gmail.com>
2022-02-23 20:32:57 -0500

    search city to pull lat and long

--- giphy-api-exercise/src/component/Search.js
+++ giphy-api-exercise/src/component/Search.js
@@ -1,8 +1,21 @@
 import React, { useState } from 'react';
 
-export const SearchCity = () =>{
+export const SearchCity = () => {
     const [city, setCity] = useState('')
 
-    const apiKey= '750f844a271e6a0e4d5caf0107a40189';
+    const apiKey = '750f844a271e6a0e4d5caf0107a40189';
 
-    const response = fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${ city }&limit=1&appid=${ apiKey }`)
+
+    return (
+        <div>
+            <input className='mr-5' onChange={(element) => setCity(element.target.value)} />
+            <button style={{ height: '40px' }} className='bg-secundary ml-5' onClick={() => {
+                const request = fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log('data', data)
+                    })
+            }} >Search</button>
+        </div>
+    )
+}
