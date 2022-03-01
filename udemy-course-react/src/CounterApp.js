16bf4d6e88708192dfe9afcc8041a81df847d6c4
Gustavo <gar.santeliz@gmail.com>
2022-02-28 19:12:34 -0500

    assigned value to reset counting

--- udemy-course-react/src/CounterApp.js
+++ udemy-course-react/src/CounterApp.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { PropTypes } from "prop-types";
 
-export const CounterApp = ({value}) =>{
+export const CounterApp = ({value = 0}) =>{
+
+    const [counter, setCounter] = useState( 0 )
+
+    const handleApp = () =>{
+        setCounter( counter + 1)
+    }
+    
+    const substractApp = () =>{
+        setCounter( counter - 1)
+    }
+
+    const resetApp = () =>{
+        setCounter(value)
+    }
+    
+    
     return(
         <>
         <h1>CounterApp</h1>
-        <h2> { value }</h2>
+        <h2> { counter }</h2>
+        {console.log(counter)}
+        <button onClick={ handleApp } >+1</button>
+        <button onClick={ resetApp }>RESET</button>
+        {/* {console.log(resetApp)} */}
+        <button onClick={ substractApp }>-1</button>
         </>
     )
 }
@@ -14,6 +35,6 @@ CounterApp.propType= {
     value: PropTypes.number.isRequired
 }
 
-CounterApp.defaultProps= {
-    value: 10
-}
+// CounterApp.defaultProps= {
+//     value: 0
+// }
