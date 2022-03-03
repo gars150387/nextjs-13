19cfac656394b2718a2dc5f3b2c1a5bad4d099f9
Gustavo <gar.santeliz@gmail.com>
2022-03-02 20:07:07 -0500

    function added

--- giphy-api-exercise/src/component/giphy/GifExpertApp.js
+++ giphy-api-exercise/src/component/giphy/GifExpertApp.js
@@ -1,1 +1,34 @@
-import React from "react";
+import { Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+
+export const GifExportApp = () =>{
+
+    // export const categories
+
+    const [ categories, setCategories] = useState([1,2,3,4,5,6])
+
+    const handleApp =()=>{
+        setCategories([...categories, ((categories.length)+1)])
+    }
+
+    return (
+        <>
+        <h2>GifExportApp</h2>
+        <hr />
+
+        <button onClick={ handleApp } >Agregar</button>
+        {categories.map(category =>{
+            return (
+                <>
+                <ul>
+                    <li key={category}>
+                        {category}
+                    </li>
+                </ul>
+                </>
+            )
+        })}
+        </>
+    )
+    
+}
