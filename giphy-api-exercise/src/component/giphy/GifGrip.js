f9a898653bf8eefc92189b45f589e64a5ff2a916
Gustavo <gar.santeliz@gmail.com>
2022-03-05 15:09:23 -0500

    animation added to loading text

--- giphy-api-exercise/src/component/giphy/GifGrip.js
+++ giphy-api-exercise/src/component/giphy/GifGrip.js
@@ -1,39 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { GifGripItem } from "./GifGripItem";
+import { useFecthGifs } from "../hooks/useFetchGifs";
 
+
+//la funcion GifGrip necesitar pasar category como argumento
 export const GifGrid = ({ category }) => {
 
-    const [images, setImages] = useState([]);
 
-    useEffect(() => {
-        getGif()
-    }, [])
+    //necesitas pasar el argumento dentro de useFetchGifs
+    const { data, loading } = useFecthGifs( category );
 
-    const getGif = async () => {
+    //todo este codigo fue trasladado a un nuevo componente de un custome hook
+    // useEffect(() => {
+        //ya que fetch function fue movida a una carpeta, el useEffect sigue manteniendo la function de getGif, 
+        //sin embargo hay que importarla desde a donde se movio, y el GetGif pasa a ser una promesa
+        // GetGif( category )
+       
+        //ademas, quedando getgif como una promesa, adicionamos el .then y traemos a setImages,
+        // .then( setImages )
 
-        const url = 'https://api.giphy.com/v1/gifs/search?q=skimboarding&limit=10&api_key=VNq7Y3Wvw1jpy05dw5wCM6wr2SK92COX';
-        const resp = await fetch(url);
-        const { data } = await resp.json();
+        //como ultimo, nos arroja un warming de dependencia faltante, lo que nos dice que si la category cambia, deseariamos que el codigo
+        //se vuelva a ejecutar, para ello, adicionamos a category dentro del array
+//    }, [ category ])
 
-        console.log(data)
-
-        const gif = data.map(img => {
-            return {
-                id: img.id,
-                user: img.user?.username,
-                url: img.images?.downsized_medium.url,
-                title: img.title
-            }
-        })
-
-        console.log(gif)
-        setImages(gif)
-    }
-    // getGif()
 
     return (
         <div>
-            {images.map(img =>
+
+            {loading ? <p className="animate__animated animate__shakeX">LOADING ...</p> : data.map(img =>
                 <GifGripItem
                     key={img.id}
                     { ...img }
