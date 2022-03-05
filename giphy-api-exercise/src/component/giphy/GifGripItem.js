67f4b9ad9984619baa54b78f676cba580494ea8b
Gustavo <gar.santeliz@gmail.com>
2022-03-05 12:55:34 -0500

    fixing problem, key deleted

--- giphy-api-exercise/src/component/giphy/GifGripItem.js
+++ giphy-api-exercise/src/component/giphy/GifGripItem.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 export const GifGripItem = ({ id, url, user, title}) => {
-
+    
+//  console.log(props)
     return (
-        <div key={id}>
+        <div >
             <h3>Title: {title}</h3>
             <img src={url} alt={url} />
             <div>User: {user}</div>
