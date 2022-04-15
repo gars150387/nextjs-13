5f0dd06ad76bcc5bf2f65a6d90fbccd70ab4934a
Gustavo <gar.santeliz@gmail.com>
2022-04-14 22:29:20 -0400

    setting context provider and routes

--- pallet305-website/src/App.js
+++ pallet305-website/src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useReducer} from 'react';
+import { AuthContext } from './auth/authContext';
 import { RouteMain } from './route/RouteMain';
+import { authReducer} from './auth/authReducer'
+
+
+const init = () => {
+  return {
+    language: true
+  }
+}
 
 
 function App() {
+
+  const [state, dispatch] = useReducer(authReducer, {} , init )
+
   return (
     <div className="App">
-      <RouteMain />
+        <AuthContext.Provider value={{
+          state,
+          dispatch
+        }}>
+          <RouteMain />
+        </AuthContext.Provider>
     </div>
   );
 }
