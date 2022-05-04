effa33a3c278183d0c46dc1a655c1417dd4b90cf
Gustavo <gar.santeliz@gmail.com>
2022-05-04 19:35:08 -0400

    config to change language added

--- calendar-app/calendar-2-app/src/components/calendar/CalendarScreen.js
+++ calendar-app/calendar-2-app/src/components/calendar/CalendarScreen.js
@@ -2,15 +2,24 @@ import React from 'react'
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 
-import { Navbar } from '../ui/Navbar'
-import 'react-big-calendar/lib/css/react-big-calendar.css'
 
+
+import { messages } from '../helper/calendar-messages-es'
+import { Navbar } from '../ui/Navbar'
+
+
+
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 import './calendarScreen.css'
+
+// import 'moment/locale/es'  // this is the config to change the language in moment
+
+// moment.locale( 'es' )
 
 const localizer = momentLocalizer(moment);
 
 const events = [{
-    title: 'cumpleanos del jefe',
+    title: 'cumpleanos de mi mama',
     start: moment().toDate(),
     end: moment().add( 2, 'hours').toDate(),
     bgcolor: '#fafafa'
@@ -19,6 +28,9 @@ const events = [{
 
 export const CalendarScreen = () => {
 
+    const eventStyleGetter = () =>{
+
+    }
     return (
         <div className='calendar-screen'>
             <Navbar />
@@ -29,6 +41,8 @@ export const CalendarScreen = () => {
                 events={ events }
                 startAccessor="start"
                 endAccessor="end"
+                messages={ messages }
+                eventStyleGetter={ eventStyleGetter}
             />
         </div>
     )
