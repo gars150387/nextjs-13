3c675a1a65cad8ac6b2cb3bfc188d5f7ceef78e3
Gustavo <gar.santeliz@gmail.com>
2022-05-05 20:27:42 -0400

    asigning properties to Calender

--- calendar-app/calendar-2-app/src/components/calendar/CalendarScreen.js
+++ calendar-app/calendar-2-app/src/components/calendar/CalendarScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 
@@ -6,15 +6,16 @@ import moment from 'moment'
 
 import { messages } from '../helper/calendar-messages-es'
 import { Navbar } from '../ui/Navbar'
+import { CalendarEvent } from './CalendarEvent'
 
 
 
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import './calendarScreen.css'
 
-// import 'moment/locale/es'  // this is the config to change the language in moment
+import 'moment/locale/es'  // this is the config to change the language in moment
 
-// moment.locale( 'es' )
+moment.locale( 'es' )
 
 const localizer = momentLocalizer(moment);
 
@@ -22,14 +23,38 @@ const events = [{
     title: 'cumpleanos de mi mama',
     start: moment().toDate(),
     end: moment().add( 2, 'hours').toDate(),
-    bgcolor: '#fafafa'
+    bgcolor: '#fafafa',
+    notes: 'comprar el pastel'
 }]
 
 
 export const CalendarScreen = () => {
 
-    const eventStyleGetter = () =>{
+    const [lastView, setlastView] = useState(localStorage.getItem( 'lastView' || 'Month')
+    )
 
+    const doubleCLick = (e) =>{
+        console.log(e)
+    }
+
+    const onSelectEvent = (e) =>{
+        console.log(e)
+    }
+
+    const onViewChange = (e) =>{
+        setlastView(e)
+        localStorage.setItem( 'lastView', e)
+    }
+
+    const eventStyleGetter = ({ event, start, end, isSelected}) =>{
+        console.log (event, start, end, isSelected)
+        const style = {
+            backgroundColor: '#367cf7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        }
     }
     return (
         <div className='calendar-screen'>
@@ -41,8 +66,16 @@ export const CalendarScreen = () => {
                 events={ events }
                 startAccessor="start"
                 endAccessor="end"
+                notes="notes"
                 messages={ messages }
-                eventStyleGetter={ eventStyleGetter}
+                eventPropGetter={ eventStyleGetter}
+                onDoubleClickEvent={ doubleCLick }
+                onSelectEvent={ onSelectEvent }
+                onView={ onViewChange }
+                view={ lastView }
+                components={{
+                    event: CalendarEvent
+                }}
             />
         </div>
     )
