1f9e2ad6d78658cd8f80077ba7bd571158f7c794
Gustavo <gar.santeliz@gmail.com>
2022-05-24 20:02:25 -0400

    redefining useEffect replacing fetch variable for createasyncthunk function

--- OneDrive/Escritorio/React/netflix-replic-app/src/components/home/Home.js
+++ OneDrive/Escritorio/React/netflix-replic-app/src/components/home/Home.js
@@ -1,9 +1,8 @@
 import React,{ useEffect } from 'react'
-import movieApi from '../../common/apis/movieApi'
 import { MovieListing } from '../movieListing/MovieListing'
-import {apikey} from '../../common/apis/movieApiKey'
 import { useDispatch } from 'react-redux'
-import { addMovies } from '../../features/movie/movieSlice'
+import { fetchAsyncMovies, fetchAsyncShows } from '../../features/movie/movieSlice'
+
 
 
 export const Home = () => {
@@ -11,19 +10,9 @@ export const Home = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
-
-    const movieText = "Harry"
-
-    const fetchMovies = async () =>{
-      const response = await movieApi
-      .get( `?apiKey=${apikey}&s=${movieText}&type=movie`)
-      .catch( (error) =>{
-        console.loog("Error :", error)
-      });
-      dispatch(addMovies( response.data ))
-    };
-    fetchMovies()
-    }, [])
+    dispatch(fetchAsyncMovies())
+    dispatch(fetchAsyncShows())
+  }, [dispatch])
   
   return (
     <div>
