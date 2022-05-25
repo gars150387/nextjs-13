514c1fd5ba0d738e38313a3669119713e55ca536
Gustavo <gar.santeliz@gmail.com>
2022-05-24 20:01:04 -0400

    setting up createasyncthunk

--- OneDrive/Escritorio/React/netflix-replic-app/src/features/movie/movieSlice.js
+++ OneDrive/Escritorio/React/netflix-replic-app/src/features/movie/movieSlice.js
@@ -1,4 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+import movieApi from '../../common/apis/movieApi'
+import {apikey} from '../../common/apis/movieApiKey'
+
+
+export const fetchAsyncMovies = createAsyncThunk(
+    'movies/fetcAsyncMovies', 
+    async () =>{
+
+        const movieText = "Harry";
+        const response = await movieApi
+        .get( `?apiKey=${apikey}&s=${movieText}&type=movie`);
+        return response.data
+})
+
+export const fetchAsyncShows = createAsyncThunk(
+    'movies/fetcAsyncShows', 
+    async () =>{
+
+        const seriesText = "friends";
+        const response = await movieApi
+        .get( `?apiKey=${apikey}&s=${seriesText}&type=series`);
+        return response.data
+})
+
 
 const initialState = {
     movies: {}
@@ -12,9 +37,31 @@ const movieSlice = createSlice({
             state.movies = payload;
         }
     },
+    extraReducers: {
+        [fetchAsyncMovies.pending]: () =>{
+            console.log("pending")
+        },
+        [fetchAsyncMovies.fulfilled]: (state, {payload}) =>{
+            console.log("fulfilled");
+            return {
+                ...state, movies: payload
+            }
+        },
+        [fetchAsyncMovies.rejected]: () =>{
+            console.log("rejected")
+        },
+        [fetchAsyncShows.fulfilled]: (state, {payload}) =>{
+            console.log("fulfilled");
+            return {
+                ...state, shows: payload
+            }
+        },
+
+    },
 })
 
 
 export const { addMovies } = movieSlice.actions;
 export const getAllMovies = (state) => state.movies.movies;
+export const getAllShows = (state) => state.movies.shows;
 export default movieSlice.reducer
