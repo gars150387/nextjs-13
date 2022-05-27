c477b80dea6124b586226487a671d54ad66b0eb4
Gustavo <gar.santeliz@gmail.com>
2022-05-27 18:57:23 -0400

    deleting duplicate file

---
+++ OneDrive/Escritorio/React/personal-website/cypress/integration/0-personal-website/button-scrollUp.js
@@ -0,0 +1,9 @@
+
+describe(" Functionality of the Scroll Up Button", () =>{
+    //checking the functionality of the button scroll up as hook
+
+    it("scroll up button", () =>{
+        cy.visit('localhost:3000')
+
+    })
+})
--- OneDrive/Escritorio/React/personal-website/cypress/integration/0-personal-website/home-page.js
+++ OneDrive/Escritorio/React/personal-website/cypress/integration/0-personal-website/home-page.js
@@ -4,6 +4,8 @@ describe('render home page', ()=>{
     it('visit home page', () =>{
 
         cy.visit('localhost:3000')
+
+        cy.contains('GARS').should('to.be.visible')
     })
 
 })
