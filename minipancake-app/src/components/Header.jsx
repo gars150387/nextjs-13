5962d3f390a405d8713c43ea18f1ae4b93a19dcb
Gustavo <gar.santeliz@gmail.com>
2022-06-26 21:02:08 -0400

    button and content header

--- minipancake-app/src/components/Header.jsx
+++ minipancake-app/src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import '../style/header.css'
+import '../style/button.css'
 
 export const Header = () => {
   return (
     <div className='header-container'>
+      <div className='header-img'></div>
+      <button className='btn shopping'>Shop</button>
 
   </div>
   )
